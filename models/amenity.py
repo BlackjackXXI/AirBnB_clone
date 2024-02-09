2ec60392c9efdf7d2526588a2bf430ddea4eac2f
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-02-09 20:18:02 +0000

    remove

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Define amenity subclass that inherits from BaseModel """
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """ Define amenity subclass that inherits from BaseModel """
-    name = ""
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Define state subclass that inherits from BaseModel """
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ Define state subclass that inherits from BaseModel """
-    name = ""
